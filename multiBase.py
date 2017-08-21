48e5fee9a21e61f2737883e274cbd4c46541f92f
Jose Reyes <lab@jreyes.es>
2017-08-21 09:13:36 +0200

    Actualizada cadena de caracteres de base para que llegue hasta base 64

--- multiBase.py
+++ multiBase.py
@@ -1,29 +1,25 @@
 #!/usr/bin python
 # coding=utf-8
-codeBase = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+codeBase = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
 #base16 = "0123456789ABCDEF"
 #base5 = "01234"
 
-
-def encode(valor,base):
+def encode(valor_en,base):
     nnum = ""
     code = codeBase[0:base]
-    while valor > 0:
-        nnum = nnum +code[int(valor%base)]
-        valor = valor/base
+    while valor_en > 0:
+        nnum = nnum +code[int(valor_en%base)]
+        valor_en = valor_en/base
+    return nnum[::-1]
 
-    cadenaCodificada = nnum[::-1]
-    return cadenaCodificada
-
-def decode(valor,base):
+def decode(valor_de,base):
     numero = 0
     c = 0
-    n = valor
+    n = valor_de
     code = codeBase[0:base]
     for i in n[::-1]:
         numero += (code.find(i)*(base**c))
         c +=1
-
     return numero
 
 if __name__ == '__main__':
